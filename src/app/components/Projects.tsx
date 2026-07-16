75da95b1138884e7d8ffa932c9565a97b5ece678
Shabeer Rehman <145320559+shabeerpk99@users.noreply.github.com>
2026-07-16 20:41:56 +0500

    Fix formatting in Projects.tsx
    Removed an extra newline in the Projects component.

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -10,7 +10,6 @@ export function Projects() {
       description: 'Enhanced an existing business networking platform by implementing production-ready frontend features, including meeting scheduling, video calling, document management with e-signature, payment workflows, role-based dashboards, and secure authentication.',
       image: '/project/nexus-project.jpg',
       tags: ['Typescript', 'React', 'vite', 'Tailwindcss', 'F2F: 123456'],
-    
       github: 'https://github.com/shabeerpk99/Nexus',
       live: 'https://nexus-bus.vercel.app/dashboard/entrepreneur'  
     },
